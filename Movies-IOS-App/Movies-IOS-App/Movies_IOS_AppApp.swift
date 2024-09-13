dc4b4ba464dbd664c4d9ecf9ff8662e720beabf7
“sohail-shaik” <sohail0411@gmail.com>
2024-09-13 12:21:20 +0530

    project creation / setup

---
+++ Movies-IOS-App/Movies-IOS-App/Movies_IOS_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movies_IOS_AppApp.swift
+//  Movies-IOS-App
+//
+//  Created by Sohail Binanzala on 13/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Movies_IOS_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
